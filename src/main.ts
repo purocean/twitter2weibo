8b4bf04efc41cfea30e6213d8ef3af7ba9437665
purocean <purocean@gmail.com>
2019-06-19 22:46:47 +0800

    调整标签解析

--- src/main.ts
+++ src/main.ts
@@ -43,14 +43,14 @@ const loop = async () => {
             }
 
             const format = async (item: twitter.Post, t: boolean) => {
-                const str = item.content.replace(/(#\S*)(\s|$)/g, '$1#')
+                const str = item.content.replace(/(#\S+)(\s|$)/g, '$1#')
                 const chinese = t ? await translate(str) : ''
                 const content = (chinese ? chinese + '\n------------\n' : '') + str
 
                 let text = '#川普推特搬运# ' + moment(item.time).tz('America/New_York').format('YYYY-MM-DD HH:mm:ssZ') + '\n'
                      + content + '\n'
                 if (item.quote) {
-                    const str = item.quote.content.replace(/(#\S*)(\s|$)/g, '$1#')
+                    const str = item.quote.content.replace(/(#\S+)(\s|$)/g, '$1#')
                     const chinese = await translate(str)
                     const content = (chinese ? chinese + '\n------------\n' : '') + str
 
